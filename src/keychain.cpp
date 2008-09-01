fe8c22a6098e1f63d906b69ca5fa1ceeb474bee0
Chris Jones <skating.tortoise@gmail.com>
2008-09-01 21:39:22 +0000

    Gnome keychain support
    git-svn-id: https://ssl.davidsansome.com/svn/feeder@93 291fe25c-c45b-4c69-963a-29b802a1c816

--- src/keychain.cpp
+++ src/keychain.cpp
@@ -2,6 +2,19 @@
 
 #ifdef Q_OS_DARWIN
 #include <Security/Security.h>
+#elif defined (Q_OS_UNIX)
+extern "C"{
+#include <gnome-keyring.h>
+}
+
+static GnomeKeyringPasswordSchema ourSchema = {
+	GNOME_KEYRING_ITEM_GENERIC_SECRET, 
+	{
+		{ "username", GNOME_KEYRING_ATTRIBUTE_TYPE_STRING },
+		{ "service", GNOME_KEYRING_ATTRIBUTE_TYPE_STRING },
+		{ NULL }
+	},
+};
 #endif
 
 #include <QDebug>
@@ -9,9 +22,9 @@
 const QString Keychain::kServiceName = "Purplehatstands-Feeder";
 
 QString Keychain::getPassword(QString account) {
+	char* password;
 #ifdef Q_OS_DARWIN
 	UInt32 password_length;
-	char* password;
 	OSStatus ret = SecKeychainFindGenericPassword(
 		NULL,
 		kServiceName.length(),
@@ -27,6 +40,22 @@ QString Keychain::getPassword(QString account) {
 	else {
 		QString pass = QString::fromAscii(password, password_length);
 		SecKeychainItemFreeContent(NULL, password);
+		return pass;
+	}
+#elif defined (Q_OS_UNIX)
+	GnomeKeyringResult result = gnome_keyring_find_password_sync(
+		&ourSchema,
+		&password,
+		"username", account.toStdString().c_str(),
+		"service", kServiceName.toStdString().c_str(),
+		NULL);
+	
+	if (result != GNOME_KEYRING_RESULT_OK) {
+		return QString::null;
+	}
+	else{
+		QString pass = QString(password);
+		gnome_keyring_free_password(password);
 		return pass;
 	}
 #else
@@ -48,6 +77,19 @@ void Keychain::setPassword(QString account, QString password) {
 
 	if (ret != 0)
 		qWarning() << "Error setting password in keychain";
+#elif defined (Q_OS_UNIX)
+    QString displayName=("Feeder Google Reader account for ");
+    displayName.append(account);
+	GnomeKeyringResult result = gnome_keyring_store_password_sync(
+		&ourSchema, NULL,
+		displayName.toStdString().c_str(),
+		password.toStdString().c_str(),
+		"username", account.toStdString().c_str(),
+		"service", kServiceName.toStdString().c_str(),
+		NULL);
+	if (result != GNOME_KEYRING_RESULT_OK){
+		qWarning() << "Error setting password in keychain";
+	}
 #else
 	return;
 #endif
